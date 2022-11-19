2b59eeaa82d89dd748f6e5908ea8dcab901a185c
Fábio_Luiz <fabiolljunior23@gmail.com>
2022-11-18 23:36:31 -0300

    Create elements.js

---
+++ elements.js
@@ -0,0 +1,25 @@
+const btnPlay = document.querySelector(".btnPlay")
+const btnPause =document.querySelector(".btnPause")
+const btnStop = document.querySelector(".btnStop")
+const btnMore = document.querySelector('.btnMore')
+const btnLess = document.querySelector(".btnLess")
+const minutesDisplay = document.querySelector(".minutes")
+const secondsDisplay = document.querySelector(".seconds")
+const btnForest = document.querySelector(".btnForest")
+const btnRain = document.querySelector(".btnRain")
+const btnCoffee = document.querySelector(".btnCoffee")
+const btnFire = document.querySelector(".btnFire")
+
+export {
+    btnPlay,
+    btnPause,
+    btnStop,
+    btnMore,
+    btnLess,
+    minutesDisplay,
+    secondsDisplay,
+    btnForest,
+    btnRain,
+    btnCoffee,
+    btnFire
+}
